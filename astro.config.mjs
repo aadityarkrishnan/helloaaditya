d54365aff26b54f628db5f9ec92f3329288b8cf9
aadityaradhakrishnan <meetarkdeveloper@gmail.com>
2024-12-30 14:04:41 +0530

    Updated configuration for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,24 @@
-// import { defineConfig } from "astro/config";
-// import tailwind from "@astrojs/tailwind";
-// import mdx from "@astrojs/mdx";
-// import sitemap from "@astrojs/sitemap";
-// import icon from "astro-icon";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   site: "https://astroship.web3templates.com",
-//   integrations: [tailwind(), mdx(), sitemap(), icon()],
-//   devToolbar: {
-//     enabled: false,
-//   },
-// });
-
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: 'https://aadityarkrishnan.github.io',
   base: '/helloaaditya',
-  integrations: [tailwind(), mdx(), sitemap()],
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    icon()
+  ],
   vite: {
-    // Add base to vite config
-    base: '/helloaaditya/'
+    build: {
+      assetsPrefix: '/helloaaditya'
+    }
   }
 });
