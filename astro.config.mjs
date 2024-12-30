b97f9802bf817a239538841fe943697ac71f78ff
aadityaradhakrishnan <meetarkdeveloper@gmail.com>
2024-12-30 12:58:58 +0530

    Prepare for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,18 @@
+// import { defineConfig } from "astro/config";
+// import tailwind from "@astrojs/tailwind";
+// import mdx from "@astrojs/mdx";
+// import sitemap from "@astrojs/sitemap";
+// import icon from "astro-icon";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: "https://astroship.web3templates.com",
+//   integrations: [tailwind(), mdx(), sitemap(), icon()],
+//   devToolbar: {
+//     enabled: false,
+//   },
+// });
+
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
@@ -6,7 +21,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://aadityarkrishnan.github.io", // Changed to your GitHub Pages domain
+  base: "/helloaaditya", // Added base path - should match your repository name
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   devToolbar: {
     enabled: false,
