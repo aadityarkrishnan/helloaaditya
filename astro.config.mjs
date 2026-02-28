f245c375443e0ac9e8bca9a9e54ebbf98fc55dd8
aadityaradhakrishnan <meetarkdeveloper@gmail.com>
2026-02-28 13:32:48 +0530

    Updated Resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 export default defineConfig({
@@ -16,7 +15,6 @@ export default defineConfig({
   integrations: [
     tailwind(),
     mdx(),
-    sitemap(),
     icon()
   ]
 });
